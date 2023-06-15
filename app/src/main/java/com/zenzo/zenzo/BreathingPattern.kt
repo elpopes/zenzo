1d049783921c71cc04af043efa79a7396fe789fe
elpopes <lorenzotijerina@gmail.com>
2023-06-15 14:16:34 -0400

    Fix Slow Breath

--- app/src/main/java/com/zenzo/zenzo/BreathingPattern.kt
+++ app/src/main/java/com/zenzo/zenzo/BreathingPattern.kt
@@ -4,5 +4,5 @@ enum class BreathingPattern(val inhale: Int, val hold: Int, val exhale: Int) {
     CALM(4, 7, 8),
     ENERGY(8, 2, 4),
     BALANCE(4, 4, 4),
-    SLOW(15, 0, 15)
+    SLOW(14, 1, 15)
 }
