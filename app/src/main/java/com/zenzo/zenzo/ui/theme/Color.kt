12190fe10ee63621093f73f43a603e5528519ca6
elpopes <lorenzotijerina@gmail.com>
2023-07-02 16:04:20 -0400

    adjust balance Color.kt

--- app/src/main/java/com/zenzo/zenzo/ui/theme/Color.kt
+++ app/src/main/java/com/zenzo/zenzo/ui/theme/Color.kt
@@ -4,5 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val LightBlue = Color(0xFF87CEFA) // Light Blue
 val EnergyRed = Color(0xFFFF0000) // Red
-val BalanceGreen = Color(0xFF00FF00) // Green
+val BalancePurple = Color(0xFF800080) // Purple
 val SlowYellow = Color(0xFFFFFF00) // Yellow
