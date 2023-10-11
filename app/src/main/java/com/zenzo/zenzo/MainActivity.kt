b2c5ce4004b5871cfd54a08d2782f48e03850f73
elpopes <lorenzotijerina@gmail.com>
2023-10-11 10:07:44 -0400

    Add breath descriptions

--- app/src/main/java/com/zenzo/zenzo/MainActivity.kt
+++ app/src/main/java/com/zenzo/zenzo/MainActivity.kt
@@ -1,32 +1,15 @@
 package com.zenzo.zenzo
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import android.os.Build
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)  // Important to call superclass method
-
-        // For newer versions
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let { controller ->
-                controller.hide(android.view.WindowInsets.Type.statusBars())
-                controller.systemBarsBehavior = android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            // For older versions
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-            supportActionBar?.hide()
-        }
-
+        super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             NavHost(navController, startDestination = "meditationOptions") {
@@ -46,5 +29,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
