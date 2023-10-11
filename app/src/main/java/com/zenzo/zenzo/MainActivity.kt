9ffa3573baada84b01b5ca95043b5c6fedc6e5a6
elpopes <lorenzotijerina@gmail.com>
2023-10-11 11:43:35 -0400

    remove time from app

--- app/src/main/java/com/zenzo/zenzo/MainActivity.kt
+++ app/src/main/java/com/zenzo/zenzo/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.zenzo.zenzo
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) // Don't forget to call super.onCreate
+
+        // Hide the status bar
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+
         setContent {
             val navController = rememberNavController()
             NavHost(navController, startDestination = "meditationOptions") {
